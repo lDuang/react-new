7b7b13f6fdbc42f1a79dd4c2cd3be238dd3998c5
Duang <duang7896@163.com>
2025-06-23 03:51:41 +0000

    fix next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,8 @@ const nextConfig: NextConfig = {
   // ... 其他配置，例如 reactStrictMode: true
   // 如果您在 Cloud Studio 等在线环境中遇到跨域警告，可以添加此项
   allowedDevOrigins: [
-    "https://qjczsa-vzpdmo-3000.app.cloudstudio.work", // 替换为您的实际域名
+    "https://coderpath.dev", // 替换为您的实际域名
+    "https://coderpath.me", // 替换为您的实际域名
     // 如果有其他开发环境的域名，也可以添加
   ],
   webpack: (config, { isServer }) => {
