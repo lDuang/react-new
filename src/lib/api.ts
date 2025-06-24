0414e83e8757bd1901a79618770503a6e7b7bb43
Duang <duang7896@163.com>
2025-06-24 08:40:28 +0000

    update

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@
 import { useAuthStore } from '@/features/auth/store';
 import { PaginatedEntries } from '../types';
 
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL!;
+export const API_BASE_URL = process.env.API_BASE_URL!;
 
 export async function fetcher<JSON>(url: string, options: RequestInit = {}): Promise<JSON> {
   const { logout } = useAuthStore.getState();
